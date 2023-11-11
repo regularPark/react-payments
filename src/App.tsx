d4240837ca5af10e6bfd5b2e2046e1dc9b62b125
regularPark <srk99279@naver.com>
2023-11-11 21:51:56 +0900

    feat: 프로젝트 생성

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from './components/common/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <div>hieee</div>
+      <Button variant="card">+</Button>
+    </div>
+  );
+}
+
+export default App;
